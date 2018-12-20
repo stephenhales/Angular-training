194e64dcbf8f0a074b6bece8cd62a5ae776f3304
stephenhales <stephen.james.hales@gmail.com>
2018-12-20 15:22:08 -0600

    demo-app - demos with RxJS observables, pipes, and intervals

--- demo-app/src/main.ts
+++ demo-app/src/main.ts
@@ -10,3 +10,55 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+
+// Observables demo
+import { interval } from 'rxjs';
+import { Observable, Observer } from 'rxjs';
+import { map, filter, take } from 'rxjs/operators';
+
+const nums: Observable<number> = Observable.create( (observer: Observer<number>) => {
+
+  let counter = 0;
+
+  setInterval(() => {
+    observer.next(counter++);
+  }, 500);
+});
+
+nums.subscribe((num) => {
+  console.log(num);
+});
+
+nums.pipe(
+  map(x => x * 2),
+  filter(x => x > 10),
+  take(5)
+).subscribe((num) => {
+  console.log('pipe: ' + num);
+});
+
+const numsTwo: Observable<number> = Observable.create( (observer: Observer<number>) => {
+
+  let counter = 0;
+
+  const h = setInterval(() => {
+    if (counter > 10) {
+      observer.error('number is too high');
+    }
+    observer.next(counter++);
+  }, 500);
+
+  setTimeout(() => {
+    clearInterval(h);
+    observer.complete();
+  }, 2000);
+});
+
+numsTwo.subscribe((num) => {
+  console.log('two: ' + num);
+});
+
+interval(500).pipe(map(x => x * 2), take(5)).subscribe(x => console.log('interval: ' + x), null, () => {
+  console.log('interval complete');
+});
